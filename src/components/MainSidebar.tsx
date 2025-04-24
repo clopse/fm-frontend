91fb65e85b8689fb4e1dc4b7c8bd9a1c01d77755
clopse <dave.s.hurley@gmail.com>
2025-04-24 23:58:26 +0100

    Update MainSidebar.tsx

--- src/components/MainSidebar.tsx
+++ src/components/MainSidebar.tsx
@@ -19,27 +19,29 @@ export default function MainSidebar({ isMobile = false, onItemClick }: MainSideb
   };
 
   return (
-    <nav className={styles.sidebarNav}>
-      <Link href={`/hotels/${hotelId}/building`} className={styles.navItem} onClick={handleClick}>
-        <Building2 size={22} />
-        <span>Building</span>
-      </Link>
-      <Link href={`/hotels/${hotelId}/safety-score`} className={styles.navItem} onClick={handleClick}>
-        <Activity size={22} />
-        <span>Safety Score</span>
-      </Link>
-      <Link href={`/hotels/${hotelId}/utilities`} className={styles.navItem} onClick={handleClick}>
-        <PlugZap size={22} />
-        <span>Utilities</span>
-      </Link>
-      <Link href={`/hotels/${hotelId}/tenders`} className={styles.navItem} onClick={handleClick}>
-        <FileText size={22} />
-        <span>Tenders</span>
-      </Link>
-      <Link href={`/hotels/${hotelId}/service-reports`} className={styles.navItem} onClick={handleClick}>
-        <ClipboardList size={22} />
-        <span>Service Reports</span>
-      </Link>
-    </nav>
+    <aside className={styles.sidebarWrapper}>
+      <nav className={styles.sidebarNav}>
+        <Link href={`/hotels/${hotelId}/building`} className={styles.navItem} onClick={handleClick}>
+          <Building2 size={22} />
+          <span>Building</span>
+        </Link>
+        <Link href={`/hotels/${hotelId}/safety-score`} className={styles.navItem} onClick={handleClick}>
+          <Activity size={22} />
+          <span>Safety Score</span>
+        </Link>
+        <Link href={`/hotels/${hotelId}/utilities`} className={styles.navItem} onClick={handleClick}>
+          <PlugZap size={22} />
+          <span>Utilities</span>
+        </Link>
+        <Link href={`/hotels/${hotelId}/tenders`} className={styles.navItem} onClick={handleClick}>
+          <FileText size={22} />
+          <span>Tenders</span>
+        </Link>
+        <Link href={`/hotels/${hotelId}/service-reports`} className={styles.navItem} onClick={handleClick}>
+          <ClipboardList size={22} />
+          <span>Service Reports</span>
+        </Link>
+      </nav>
+    </aside>
   );
 }
