91f823b5ca09a53d38678c77da260a729a8c85dc
clopse <dave.s.hurley@gmail.com>
2025-04-24 16:43:11 +0100

    Create UtilitiesGraphs.tsx

---
+++ src/components/UtilitiesGraphs.tsx
@@ -0,0 +1,9 @@
+export function UtilitiesGraphs() {
+  return (
+    <div>
+      <h3>Utilities Comparison Graph</h3>
+      {/* You can use any charting library (e.g., Chart.js or Recharts) to visualize data */}
+      <p>Graph for gas, electricity, and water usage per guest across hotels</p>
+    </div>
+  );
+}
