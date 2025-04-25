26fea1726f78af7546b1eed8eadb445f1f6c80fa
clopse <dave.s.hurley@gmail.com>
2025-04-25 01:17:03 +0100

    Update MainSidebar.tsx

--- src/components/MainSidebar.tsx
+++ src/components/MainSidebar.tsx
@@ -1,9 +1,9 @@
-// components/MainSidebar.tsx
 'use client';
 
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
 import { ClipboardList, Activity, PlugZap, FileText, Building2 } from 'lucide-react';
+import Image from 'next/image';
 import styles from '@/styles/MainSidebar.module.css';
 
 interface MainSidebarProps {
@@ -14,28 +14,38 @@ interface MainSidebarProps {
 export default function MainSidebar({ isMobile = false, onItemClick }: MainSidebarProps) {
   const { hotelId } = useParams();
 
+  const handleClick = () => {
+    if (onItemClick) onItemClick();
+  };
+
   return (
-    <aside className={styles.sidebarNav}>
-      <Link href={`/hotels/${hotelId}/building`} className={styles.navItem} onClick={onItemClick}>
-        <Building2 size={22} />
-        <span>Building</span>
-      </Link>
-      <Link href={`/hotels/${hotelId}/safety-score`} className={styles.navItem} onClick={onItemClick}>
-        <Activity size={22} />
-        <span>Safety Score</span>
-      </Link>
-      <Link href={`/hotels/${hotelId}/utilities`} className={styles.navItem} onClick={onItemClick}>
-        <PlugZap size={22} />
-        <span>Utilities</span>
-      </Link>
-      <Link href={`/hotels/${hotelId}/tenders`} className={styles.navItem} onClick={onItemClick}>
-        <FileText size={22} />
-        <span>Tenders</span>
-      </Link>
-      <Link href={`/hotels/${hotelId}/service-reports`} className={styles.navItem} onClick={onItemClick}>
-        <ClipboardList size={22} />
-        <span>Service Reports</span>
-      </Link>
+    <aside className={styles.sidebarWrapper}>
+      <div className={styles.logoContainer}>
+        <Image src="/jmk-logo.png" alt="JMK Logo" width={120} height={50} />
+      </div>
+
+      <nav className={styles.sidebarNav}>
+        <Link href={`/hotels/${hotelId}/building`} className={styles.navItem} onClick={handleClick}>
+          <Building2 size={22} />
+          <span>Building</span>
+        </Link>
+        <Link href={`/hotels/${hotelId}/safety-score`} className={styles.navItem} onClick={handleClick}>
+          <Activity size={22} />
+          <span>Safety Score</span>
+        </Link>
+        <Link href={`/hotels/${hotelId}/utilities`} className={styles.navItem} onClick={handleClick}>
+          <PlugZap size={22} />
+          <span>Utilities</span>
+        </Link>
+        <Link href={`/hotels/${hotelId}/tenders`} className={styles.navItem} onClick={handleClick}>
+          <FileText size={22} />
+          <span>Tenders</span>
+        </Link>
+        <Link href={`/hotels/${hotelId}/service-reports`} className={styles.navItem} onClick={handleClick}>
+          <ClipboardList size={22} />
+          <span>Service Reports</span>
+        </Link>
+      </nav>
     </aside>
   );
 }
