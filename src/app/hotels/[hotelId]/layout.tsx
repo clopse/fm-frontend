0c2f299e364dec2360d1d9cccc49c1a8d60b27f2
clopse <dave.s.hurley@gmail.com>
2025-04-28 14:03:26 +0100

    Create layout.tsx

---
+++ src/app/hotels/[hotelId]/layout.tsx
@@ -0,0 +1,8 @@
+// src/app/(dashboard)/layout.tsx
+'use client';
+
+import MainLayout from '@/components/MainLayout';
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return <MainLayout>{children}</MainLayout>;
+}
