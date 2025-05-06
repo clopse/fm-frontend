14621070bf743192932c1a14ed9a847a4534a1bf
clopse <dave.s.hurley@gmail.com>
2025-05-07 00:02:59 +0100

    Update layout.tsx

--- src/app/hotels/[hotelId]/layout.tsx
+++ src/app/hotels/[hotelId]/layout.tsx
@@ -1,8 +1,18 @@
 // /src/app/hotels/[hotelId]/layout.tsx
 'use client';
 
-import HotelLayout from '@/components/HotelLayout';
+import Sidebar from '@/components/Sidebar';
+import Header from '@/components/Header';
+import styles from '@/styles/HotelLayout.module.css';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <HotelLayout>{children}</HotelLayout>;
+  return (
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <div className={styles.mainContent}>
+        <Header />
+        <main className={styles.content}>{children}</main>
+      </div>
+    </div>
+  );
 }
