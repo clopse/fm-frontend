39cb0f8857d22b7644a75ad0dd70d58cbe3500f1
clopse <dave.s.hurley@gmail.com>
2025-05-07 00:21:04 +0100

    Update layout.tsx

--- src/app/hotels/[hotelId]/layout.tsx
+++ src/app/hotels/[hotelId]/layout.tsx
@@ -1,5 +1,16 @@
 'use client';
 
+import MainSidebar from '@/components/MainSidebar';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <div style={{ display: 'flex', minHeight: '100vh' }}>
+      <aside style={{ width: '200px', backgroundColor: '#001f3f', color: 'white' }}>
+        <MainSidebar />
+      </aside>
+      <main style={{ flexGrow: 1, padding: '2rem' }}>
+        {children}
+      </main>
+    </div>
+  );
 }
