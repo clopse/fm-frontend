89cde75fd1bc8c3b7320d017eeb643099d8275bd
clopse <dave.s.hurley@gmail.com>
2025-05-07 00:06:50 +0100

    Update layout.tsx

--- src/app/hotels/[hotelId]/layout.tsx
+++ src/app/hotels/[hotelId]/layout.tsx
@@ -1,16 +1,23 @@
 // /src/app/hotels/[hotelId]/layout.tsx
 'use client';
 
-import Sidebar from '@/components/Sidebar';
-import Header from '@/components/Header';
+import MainSidebar from '@/components/MainSidebar';
+import HeaderBar from '@/components/HeaderBar';
 import styles from '@/styles/HotelLayout.module.css';
+import { useParams } from 'next/navigation';
+import { hotelNames } from '@/data/hotelMetadata';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { hotelId } = useParams();
+  const currentHotelName = hotelNames[hotelId as keyof typeof hotelNames] || 'Current Hotel';
+
   return (
     <div className={styles.wrapper}>
-      <Sidebar />
+      <aside className={styles.sidebar}>
+        <MainSidebar />
+      </aside>
       <div className={styles.mainContent}>
-        <Header />
+        <HeaderBar currentHotelName={currentHotelName} />
         <main className={styles.content}>{children}</main>
       </div>
     </div>
