d77e53eba76e733e5bbe6ef09055d70e0f891a3d
clopse <dave.s.hurley@gmail.com>
2025-05-12 01:28:08 +0100

    Update page.tsx

--- src/app/hotels/[hotelId]/compliance/page.tsx
+++ src/app/hotels/[hotelId]/compliance/page.tsx
@@ -219,7 +219,7 @@ const CompliancePage = ({ params }: Props) => {
         onClick={() => setFiltersOpen(!filtersOpen)}
         title="Show filters"
       >
-        <img src="/icons/filter-icon.png" width={25} height={25} alt="Filter" />
+        <img src="/icons/filter-icon.png" width={27} height={27} alt="Filter" />
       </button>
 
       {filtersOpen && (
