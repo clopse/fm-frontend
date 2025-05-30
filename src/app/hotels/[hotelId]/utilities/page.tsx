a72d484ccacba5e5b6782a01353d56883ec10264
clopse <dave.s.hurley@gmail.com>
2025-05-30 07:22:21 +0100

    Update page.tsx

--- src/app/hotels/[hotelId]/utilities/page.tsx
+++ src/app/hotels/[hotelId]/utilities/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState,useMemo } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useParams } from "next/navigation";
 import {
   BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend,
