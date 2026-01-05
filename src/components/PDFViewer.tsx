dbeb485bcca744b32a57bce624758aa528ec661d
clopse <dave.s.hurley@gmail.com>
2026-01-05 15:55:02 +0000

    Update PDFViewer.tsx

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -18,6 +18,12 @@ import {
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url
+).toString();
+
+
 interface PDFViewerProps {
   filePath: string;
   hotelId: string;
